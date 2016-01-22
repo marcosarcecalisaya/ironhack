99f25ab032d33a6445e0655ae3a1fa0f75bcd45f
Marcos Arce <molinox@yahoo.com>
2016-01-22 15:13:12 +0100

    Corregido el problema con los espacios y con la zeta

--- RUBY/solucion3.rb
+++ RUBY/solucion3.rb
@@ -3,9 +3,16 @@
 # end
 
 
-def solve_cipher(cadena)
- cadena.split(//).map {|a| a= (a.ord-1).chr}.join()
+def solve_cipher(cadena, param=1)
+  cadena.split(//).map  do |a|
+    unless a== " " then a= (a.ord-param).chr
+    else
+      a= " "
+    end
+    end.join()
+
 end
 
-print "#{solve_cipher("ifmmp")}\n"
+print "#{solve_cipher("p| uhdo qdph lv grqdog gxfn")}\n"
+print "#{solve_cipher("ifmmp{")}\n"
 
